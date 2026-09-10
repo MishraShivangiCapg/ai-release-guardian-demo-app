5dabe4fb106d192ef512a9696e80ba15f87a591e
MishraShivangiCapg <mishra.shivangi@capgemini.com>
2026-09-10 12:01:53 +0530

    Added customer validation during checkout

--- CheckoutController.java
+++ CheckoutController.java
@@ -1,6 +1,13 @@
 public class CheckoutController {
 
     public String checkout() {
+
+        validateCustomer();
+
         return "Success";
+    }
+
+    private void validateCustomer() {
+        System.out.println("Customer validated");
     }
 }
